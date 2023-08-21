bc49e54b498879ce69f59fe0fe2c2cc188a81fb0
Bishwo Bista <bishwo5bista@gmail.com>
2023-08-21 19:22:54 +0545

    first init

---
+++ newsletter/flashSale/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+// import appp.scss
+import "./app.scss";
+const App = () => {
+  return (
+    <>
+      <div className="main">flash sale shop asap</div>
+      <div className="container">
+        <div className="top">
+        <div className="triangle1"></div>
+        <div className="triangle2"></div>
+        <div className="triangle3"></div>
+        <div className="triangle4"></div>
+        </div>
+        <div className="bottom">
+        <div className="triangle5"></div>
+        <div className="triangle6"></div>
+        <div className="triangle7"></div>
+        <div className="triangle8"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
